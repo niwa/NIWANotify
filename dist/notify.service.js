0e739af6e923e3a9c07c0cd7ad461573e2b29567
uwe duesing <uwe.duesing@gmail.com>
2015-10-14 10:53:06 +1300

    Fix for colours and no messages

--- dist/notify.service.js
+++ dist/notify.service.js
@@ -6,27 +6,48 @@ notify
                 var deferred = $q.defer();
                 $http.get('/notifyProxy.php').then(function (response) {
 
-                    if (window.DOMParser)
-                    {
-                        parser=new DOMParser();
-                        xmlDoc=parser.parseFromString(response.data,"text/xml");
+                    if (window.DOMParser) {
+                        parser = new DOMParser();
+                        xmlDoc = parser.parseFromString(response.data, "text/xml");
                     }
                     else // Internet Explorer
                     {
-                        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-                        xmlDoc.async=false;
+                        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                        xmlDoc.async = false;
                         xmlDoc.loadXML(response.data);
                     }
                     appMessageContainer = xmlDoc.getElementsByTagName("app_message");
-                    appMessage = appMessageContainer[0].getElementsByTagName("status_text")[0].childNodes[0].nodeValue;
-                    status = appMessageContainer[0].getAttribute('status');
+                    var status = '';
+                    var appstatus = appMessageContainer[0].getAttribute('status');
+                    switch (appstatus) {
+
+                        case 'ok':
+                            status = 'ok';
+                            break;
+                        case 'message':
+                            status = 'info';
+                            break;
+                        case 'maintenance':
+                            status = 'warning';
+                            break;
+                        case 'application_error':
+                            status = 'danger';
+                            break;
+                        default:
+                            status = 'ok';
+                            break;
+                    }
+                    var appMessage = '';
+                    if (status != "ok") {
+                        appMessage = appMessageContainer[0].getElementsByTagName("status_text")[0].childNodes[0].nodeValue;
+                    }
                     appMsg =
                     {
-                        msg:appMessage,
+                        msg: appMessage,
                         status: status
-                    }
+                    };
                     deferred.resolve(appMsg);
-                })
+                });
                 return deferred.promise;
 
             }
